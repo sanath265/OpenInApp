0fc727e197ca0c52086fafa8135ed0ef78fccd9f
sanath265 <venkatasaisanathnar@airasia.com>
2023-07-07 14:55:28 +0530

    Initial Commit

---
+++ OpenInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenInApp
+//
+//  Created by Sanath on 07/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
